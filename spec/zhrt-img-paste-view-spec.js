0b542a1c9042a2812ab4e0bb70f83fde905de226
zhrt123 <1446384557@qq.com>
2019-09-23 21:02:18 +0800

    zhrt

--- spec/zhrt-img-paste-view-spec.js
+++
@@ -1,9 +0,0 @@
-'use babel';
-
-import ZhrtImgPasteView from '../lib/zhrt-img-paste-view';
-
-describe('ZhrtImgPasteView', () => {
-  it('has one valid test', () => {
-    expect('life').toBe('easy');
-  });
-});
